7f35ac273858ec8085561c515a446229b287ecef
”Vladislav <ra1aio@ya.ru>
2023-01-10 18:48:50 +0300

    correction 1

--- KataCalc/src/Main.java
+++ KataCalc/src/Main.java
@@ -5,42 +5,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         sc.close();
+        System.out.println(calc(input));
+    }
+    public static String calc(String input) throws WrongDataException{
         String[] inputArr = input.trim().split(" ");
         dataVerification(inputArr);
-        System.out.println(calculation(inputArr));
-
-
-
-    }
-
-    private static void dataVerification(String[] userInput) throws WrongDataException {
-
-        //В строке лишние символы
-        if (userInput.length != 3) {
-            throw new WrongDataException("Строка не является математической операцией");
-        }
-        try {
-            int num1 = Integer.parseInt(userInput[0]);
-            int num2 = Integer.parseInt(userInput[2]);
-            String operator = userInput[1];
-            //Одно из чисел больше 10
-            if (num1 > 10 || num2 > 10) {
-                throw  new WrongDataException("Числа не могут быть больше 10");
-            }
-            // Деление на ноль
-            if (num2 == 0 & operator.equals("/")) {
-                throw new ArithmeticException("Деление на ноль");
-            }
-            // Ошибка при парсинге в число
-        } catch (NumberFormatException e) {
-            throw new NumberFormatException("Неверный ввод одного из чисел");
-        }
-    }
-    private static int calculation(String[] userInput) throws WrongDataException {
-        int num1 = Integer.parseInt(userInput[0]);
-        int num2 = Integer.parseInt(userInput[2]);
-        String operator = userInput[1];
-        int res = 0;
+        int num1 = Integer.parseInt(inputArr[0]);
+        int num2 = Integer.parseInt(inputArr[2]);
+        String operator = inputArr[1];
+        int res;
         switch (operator) {
             case "+" :
                 res = num1 + num2;
@@ -57,6 +30,27 @@ public class Main {
             default:
                 throw new WrongDataException("Недопустимый оператор");
         }
-        return res;
+        return String.valueOf(res);
     }
+
+    private static void dataVerification(String[] userInput) throws WrongDataException {
+
+        //В строке лишние символы
+        if (userInput.length != 3) {
+            throw new WrongDataException("Строка не является математической операцией");
+        }
+        try {
+            int num1 = Integer.parseInt(userInput[0]);
+            int num2 = Integer.parseInt(userInput[2]);
+            //String operator = userInput[1];
+            //Одно из чисел меньше 0 или больше 10
+            if ((num1 > 10 || num2 > 10) || (num1 < 1 || num2 < 1)) {
+                throw  new WrongDataException("Числа не могут быть меньше 0 или больше 10");
+            }
+            // Ошибка при парсинге в число
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("Неверный ввод одного из чисел");
+        }
+    }
+
 }
